ea66a78d2a75c92a8d93ca7406ee08fa182a9edc
Sadashiv Byagi_ons <43012335+SadashivByagi@users.noreply.github.com>
2023-09-22 09:46:41 +0100

    First commit..

---
+++ src/com/test/java/JavaTestscripts.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+public class JavaTestscripts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is demo for our CoP test community");
+
+	}
+
+}
